8ea4c043bde3679aa84e045b3153c9f59b3f5a36
INNO <2403punjabidivya@gmail.com>
2024-10-08 15:23:27 +0530

    initial commit

---
+++ src/wrapper/AppWrapper.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+// import { Outlet } from 'react-router-dom'
+
+const AppWrapper = () => {
+  return (
+    <
+    // <Header/>
+    // <Outlet/>
+    // <Footer/>
+    // </>
+  )
+}
+
+export default AppWrapper
