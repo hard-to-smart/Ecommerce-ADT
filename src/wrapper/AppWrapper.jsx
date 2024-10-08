e476da6c53f84167a2bb228f5547b72b38cd229e
hard-to-smart <2403punjabidivya@gmail.com>
2024-10-09 00:49:37 +0530

    footer component added

--- src/wrapper/AppWrapper.jsx
+++ src/wrapper/AppWrapper.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
 
 const AppWrapper = () => {
   return (
     <>
-    {/* <Header/> */}
+    <Header/>
     <Outlet/>
-    {/* <Footer/> */}
+    <Footer/>
     </>
   )
 }
