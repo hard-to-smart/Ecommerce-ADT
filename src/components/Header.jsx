a76e8cb0b219a4702427500f284f7047939d3003
hard-to-smart <2403punjabidivya@gmail.com>
2024-10-09 07:29:15 +0530

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,35 @@
 import React from "react";
 import logo from "../assets/logo.png";
-import { NavLink } from 'react-router-dom'
-import navbg from "../assets/navbg.jpg"
+import { NavLink } from 'react-router-dom';
+import navbg from "../assets/navbg.jpg";
 
 const Header = () => {
   return (
-    <header className=" px-4 py-2  text-white bg-gray-900 flex flex-row justify-between shadow-lg bg-cover -mx" 
-    // style={{backgroundImage:`url(${navbg})`}}
-    >
-      <NavLink to='/'>
-        <img src={logo} className="w-10 h-10" />
+    <header className="relative flex items-center justify-between px-4 py-4 text-white bg-gray-900 bg-opacity-50 shadow-lg">
+      <div className="absolute inset-0">
+        <img src={navbg} className="object-cover w-full h-full opacity-50" />
+      </div>
+      
+      <div className="absolute inset-0 bg-black opacity-40"></div>
+
+      <NavLink to="/" className="relative z-10">
+        <img src={logo} alt="Logo" className="w-10 h-10" />
       </NavLink>
-      <ul>
-        <NavLink to='/product'> Products</NavLink>
-        <NavLink to='/blog'> Blogs</NavLink>
-        <NavLink to='/cart'>Cart</NavLink>
+
+      <ul className="relative z-10 flex space-x-6">
+        <li>
+          <NavLink to="/product" className="hover:text-white">Products</NavLink>
+        </li>
+        <li>
+          <NavLink to="/blog" className="hover:text-white">Blogs</NavLink>
+        </li>
+        <li>
+          <NavLink to="/cart" className="hover:text-white">Cart</NavLink>
+        </li>
       </ul>
-      <div>
-        <NavLink to='/profile'>Profile</NavLink>
+
+      <div className="relative z-10">
+        <NavLink to="/profile" className="hover:text-gray-300">Profile</NavLink>
       </div>
     </header>
   );
